45dec970e683f9a7b2bd83dff962029c7f6a8884
PeGa! <dev@pega.sh>
2017-08-09 12:10:56 -0300

    Adds script for deployingi new repositories for debian stretch

---
+++ debian-stretch/scripts/install-repos.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+errorHandling() {
+    echo "Unable to continue. Error on line $1"
+}
+
+alreadyInstalled() {
+	echo 
+	echo "	Installation aborted. I found previous backups;"
+	echo "	repositories already installed."
+	echo
+	exit 1
+}
+
+noStoredConfigFound(){
+	echo
+	echo "Unable to find settings to deploy."
+	echo "Please make sure you're running this script from"
+	echo "its own directory and that a conf/ directory"
+	echo "containing all the stored settings for apt exists"
+	echo "at the same level of the scripts/ directory"
+	echo
+	exit 1
+}
+
+trap 'errorHandling $LINENO' ERR
+
+mainSources="sources.list"
+additionalSources="sources.list.d"
+aptDir="/etc/apt"
+renamedSuffix="original.disabled"
+storedConf="../conf/apt"
+
+# Running sanity checks / requirements
+if ! [ -f $(pwd)/$(basename $0) ]; then
+	echo "Please run this script from its own directory."
+	exit 1
+fi
+[ -f $aptDir/$mainSources.$renamedSuffix ] && alreadyInstalled
+[ -d $aptDir/$additionalSources.$renamedSuffix ] && alreadyInstalled
+[ -f $storedConf/$mainSources ] || noStoredConfigFound
+[ -d $storedConf/$additionalSources ] || noStoredConfigFound
+
+# Backup current settings
+mv $aptDir/$mainSources $aptDir/$mainSources.$renamedSuffix
+mv $aptDir/$additionalSources $aptDir/$additionalSources.$renamedSuffix
+
+cp $storedConf/$mainSources $aptDir/
+cp -r $storedConf/$additionalSources $aptDir/
+
+apt update
+
+echo 
+echo "Repos updated."
